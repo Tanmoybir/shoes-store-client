602fb6c0aec25f2a50eefcadf2a5a192e9703547
Tanmoy Bir <tanmoy7468@gmail.com>
2024-03-18 23:05:45 +0600

    created auth provider for auth context

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import myCreateRoute from './route/myCreateRoute'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import AuthProvider from './provider/AuthProvider'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+    <AuthProvider>
     <RouterProvider router={myCreateRoute}/>
+    </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 )
