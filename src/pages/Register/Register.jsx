1b3626d2afe739b3d1fd4559dfecbb9975d7aa4f
Tanmoy Bir <tanmoy7468@gmail.com>
2024-03-18 22:43:48 +0600

    log in form created and input value set on use state

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -46,7 +46,7 @@ const Register = () => {
                         </p>
                         <Link to={'/login'}>
                             <button className="inline-flex items-center justify-center rounded-md text-sm font-medium  h-10 px-4 py-2 w-full mb-2 bg-black text-white">
-                                CREATE ACCOUNT
+                                Sign In
                             </button>
                         </Link>
                         <p className="text-center my-4">OR</p>
